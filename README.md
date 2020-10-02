691a50fdf3fcd1bd0f2c5d12341db2d42f2b906a
Travis Lawrence <travis.lawrence12@gmail.com>
2020-10-02 13:08:00 -0700

    Here it is

---
+++ README.md
@@ -0,0 +1,11 @@
+Download all the results from : https://drive.google.com/drive/u/0/folders/1RtlftwSAvzLRdTmpspko-ejrwEhGO6Rl
+
+And the team photos I used are from: https://drive.google.com/drive/u/0/folders/1ODNhKWiAyPxDH5O7ac2Lx8GAW4lv7H1_
+
+I had to take two photos of each team because one player is always highlighted which makes their image bigger. So for my script you have to separate these two team photos into a `./teams` and a `./first_players` folders.
+
+Make sure a `./updated` directory als exists for the final images.
+
+The coordinates in the code are based on 4k image resolution images (3840 × 2160 pixels) of the teams.
+
+Run `yarn install`, and then then run `node index.js` from command line to run.
---
+++ index.js
@@ -0,0 +1,50 @@
+const sharp = require('sharp');
+const fs = require('fs');
+const path = require('path');
+
+const colGap = 53;
+
+// Left, top
+const firstRow = [410, 368];
+const secondRow = [986, 902];
+const thirdRow = [410, 1436];
+
+const width = 331;
+const height = 490;
+
+getImages('teams', 1); // gets all of the players excpet first player (who is highlighted)
+getImages('first_players', 0, 0); // gets that first player
+
+function getImages(folder, start = 0, end = 20) {
+  console.log(`start ${folder}`);
+  const directoryPath = path.join(__dirname, `./${folder}`);
+
+  fs.readdir(directoryPath, function (err, files) {
+    if (err) {
+      return console.log('Unable to scan directory: ' + err);
+    }
+
+    files.forEach(function (file, idx) {
+      for (let i = start; i <= end; i++) {
+        let left, top, itemLeft;
+        if (i < 8) {
+          [left, top] = firstRow;
+          itemLeft = left + (width + colGap) * i;
+        } else if (i < 13) {
+          [left, top] = secondRow;
+          itemLeft = left + (width + colGap) * (i - 8);
+        } else {
+          [left, top] = thirdRow;
+          itemLeft = left + (width + colGap) * (i - 13);
+        }
+
+        sharp(`./${folder}/${file}`)
+          .extract({ left: itemLeft, top, width, height })
+          .png({ adaptiveFiltering: true })
+          .toFile(`./updated/${idx}-${i}.png`, (err) => {
+            console.log(err);
+          });
+      }
+    });
+  });
+}
