4ea09bbe4f0ed6d86d45dc3286d3fa1744864b16
Travis Lawrence <travis.lawrence12@gmail.com>
2021-04-25 14:38:40 -0700

    preview up load

--- src/public/scripts/application.js
+++ src/public/scripts/application.js
@@ -1,5 +1,6 @@
 const imgForm = document.getElementById('image-form');
 const imgGrid = document.querySelector('.image-grid');
+const uploadContainer = document.querySelector('.image-upload');
 let files;
 
 const submitImage = async () => {
@@ -25,6 +26,17 @@ const submitImage = async () => {
 
 imgForm.file.addEventListener('change', (event) => {
   files = event.target.files[0];
+  const reader = new FileReader();
+
+  reader.onload = (e) => {
+    const imgContainer = document.createElement('div');
+    imgContainer.classList = 'uploadContainer';
+    const img = document.createElement('img');
+    img.src = e.target.result;
+    imgContainer.append(img);
+    uploadContainer.append(imgContainer);
+  };
+  reader.readAsDataURL(files);
 });
 
 imgForm.addEventListener('submit', (event) => {
