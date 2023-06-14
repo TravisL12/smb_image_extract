9fc95aa9de4c894098c000470830e114a2220e58
Travis Lawrence <travis.lawrence12@gmail.com>
2023-06-14 16:34:17 -0700

    last thing

--- imageParse.js
+++ imageParse.js
@@ -1,33 +1,30 @@
 const sharp = require("sharp");
 const fs = require("fs");
 const path = require("path");
+const { snakeCase } = require("lodash");
+
+const smb3_lineups = require("./smb3_lineups/smb3_complete_lineup.json");
+const smb4_lineups = require("./smb4_complete_lineup.json");
 
 const { HEIGHT, WIDTH, CARD_SIZES } = require("./constants.js");
-
-// uncomment one!
-const smb4_lineups = require("./smb4_complete_lineup.json");
-const smb3_lineups = require("./smb3_lineups/smb3_complete_lineup.json");
-const { snakeCase } = require("lodash");
-function getSmb3Teams() {
-  return smb3_lineups;
-}
-
-const getSmb4Teams = () => {
-  return smb4_lineups;
-};
-
-const game = "smb4";
-const teams = getSmb4Teams();
-const inputDir = "support/smb4_teams";
-
-const VALID_IMG_TYPES = [".png", ".jpg", ".JPEG"];
 
 function round(num) {
   return Math.round(num);
 }
 
+const game = "smb4";
+const data = {
+  smb3: { lineup: smb3_lineups, inputDir: "support/smb3_teams" },
+  smb4: { lineup: smb4_lineups, inputDir: "support/smb4_teams" },
+};
+const teams = data[game].lineup;
+const inputDir = data[game].inputDir;
+
+const VALID_IMG_TYPES = [".png", ".jpg", ".JPEG"];
+
 const { gap, card, firstCard, row1, row2, row3, playerCount, rowCount } =
   CARD_SIZES[game];
+
 function getSizes(screenWidth, screenHeight) {
   const colGap = Math.floor((gap / WIDTH) * screenWidth);
   const firstRow = [
